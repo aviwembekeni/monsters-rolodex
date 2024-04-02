d243b03ba0b34a67f51a9bd9e824cbcc8e1155ef
Aviwe Ngqukumba <aviwe3226@gmail.com>
2024-04-02 05:53:27 -0700

    Implemented search filter by monter name

--- src/App.js
+++ src/App.js
@@ -1,38 +1,58 @@
-import {React , Component } from 'react'
+import { React, Component } from "react";
 
-import './App.css';
+import "./App.css";
 
 class App extends Component {
-
-  constructor(){
+  constructor() {
     super();
     this.state = {
       monsters: [],
-
-    }
+      searchField: "",
+    };
   }
 
-  componentDidMount(){
-    fetch('https://jsonplaceholder.typicode.com/users')
-    .then(response => response.json())
-    .then(users => {
-      this.setState(()=> {
-        return {
-          monsters: users
-        };
-      }
-      ,()=> console.log(this.state)
-      )
-    })
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) =>
+        this.setState(
+          () => {
+            return {
+              monsters: users,
+            };
+          },
+          () => console.log(this.state)
+        )
+      );
   }
 
-  render(){
-    const { monsters } =  this.state;
+  render() {
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter((monster) => {
+      return monster.name?.toLowerCase().includes(searchField?.toLowerCase());
+    });
+    console.log(filteredMonsters);
+
     return (
       <div className="App">
-        {
-          monsters.map(monster => <h1 key={monster.id}> {monster.name} </h1>)
-        }
+        <input
+          name="search-box"
+          type="search"
+          placeholder="search monsters"
+          onChange={(e) => {
+            this.setState(
+              () => {
+                return {
+                  searchField: e.target.value.toLowerCase(),
+                };
+              },
+              () => console.log(this.state)
+            );
+          }}
+        />
+        {filteredMonsters.map((monster) => (
+          <h1 key={monster.id}> {monster.name} </h1>
+        ))}
       </div>
     );
   }
